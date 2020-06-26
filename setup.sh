36fc1553a07a13db439799bb668c43480eacfac7
coolabhays <abhaysp9955@gmail.com>
2020-06-26 07:54:55 +0530

    install program and installation setup

--- setup.sh
+++ setup.sh
@@ -49,6 +49,7 @@ function check_update() {
 	update_errcd=1
 	if ! update_upgrade; then
 		update_errcd="$?"
+		echo "error ${update_errcd}"
 		count=$(( count + 1 ))
 		echo -e $R"Updation failed$W, check if internet connection is active"
 		echo "It's recommended to update if it's the first time"
@@ -62,31 +63,84 @@ function check_update2() {
 		echo -e $G"Update Succesful"
 	else
 		echo "Try again or Skip"
-		echo -e $G"y$W or$R n$W"
+		echo -e $G"y$W or$R s$W"
 		read -rn1 update_again
 	fi
 }
 check_update2
+echo "${update_again}"
 
-if [ "${update_again}" != "" ]; then
-	while [[ "${update_errcd}" -ne 1 || "${count}" -le 3 ]]; do
-		if [ "${udpate_again}" = "y" ]; then
+if [ -n "${update_again}" ]; then
+	#echo "inside if"
+	#echo "${update_again}"
+	while [[ "${update_errcd}" -ne 1 && "${count}" -le 3 ]]; do
+		if [ "${update_again}" = "y" ]; then
+			#echo "inside while if"
 			check_update
 			check_update2
+			#echo "${count}"
+		else
+			echo "You choose to skip"
+			break
 		fi
+		#echo "inside while"
+		#echo "${update_again}"
 	done
-	if [ "${count}" -eq 3 ]; then
+	if [ "${count}" -ge 3 ]; then
 		echo -e $R"Skipping$W for Now, or do you want to quit"
 		echo -e $G"s$W or$R q$W"
 		read -rn1 if_skip
 		if [ "${if_skip}" = q ]; then
+			echo -e $Y"Exiting Now, BYE$W"
 			exit
 		else
-			echo $G"SKIPPING$G"
+			echo "Tried, more than 3 times. NOW, "
+			echo -e $G"SKIPPING$W"
 		fi
 	fi
 fi
 
-echo
-echo "DONE"
-echo
+echo -e $Y"Setting up storage settings"
+echo -e $G"Grant the permission"
+
+if termux-storage-setup; then
+	echo -e $Y"Storage setup successful"
+	echo -e $W"Created a folder named storage"
+else
+	echo -e $R"Storage setup failed, proceeding further$W"
+fi
+
+if [ "${update_errcd}" -ne 0 ]; then
+	echo -e $G"Installing some basic softwares$W"
+	ins_pro=0
+else
+	ins_pro=1
+	echo -e $R"Program installations has been skipped"
+fi
+
+function install_programs() {
+	echo -e $Y"Installing text editors$W"
+	apt install -y neovim nano
+
+	echo -e $Y"Installing language & tools$W"
+	apt install -y coreutils python3 make libllvm llvm libcrypt-dev
+
+	echo -e $Y"Installing tools to retrieve and transfer data"
+	apt install -y curl wget aria2
+
+	echo -e $Y"Installing a nice file manager"
+	apt install -y lf
+
+	echo -e $Y"Installing termux specific tools"
+	apt install -y termux-exec proot
+
+	echo -e $Y"Installing ssh suite"
+	apt install -y ssh
+}
+
+if [ "${ins_pro}" -eq 0 ]; then
+	install_programs
+fi
+
+echo -e $G"For further setup, type$Y 'termux-setup'$W"
+echo -e $Y"Exiting Now"
